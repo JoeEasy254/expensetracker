b9f42f12f199366af2699fcd162b23faa104ebed
joseph-muchene <ngugimuchene@gmail.com>
2024-12-15 18:57:22 +0300

    initial commit

--- src/config.js
+++ src/config.js
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyBs_KejLFBmmSSF0KQd61jIwRnngk6Kbcc",
-  authDomain: "expense-tracker-8d0a3.firebaseapp.com",
-  projectId: "expense-tracker-8d0a3",
-  storageBucket: "expense-tracker-8d0a3.firebasestorage.app",
-  messagingSenderId: "730324675163",
-  appId: "1:730324675163:web:a7eb0a8f8f959910872d7e",
+  // copy you configurations
 };
 
 // Initialize Firebase
